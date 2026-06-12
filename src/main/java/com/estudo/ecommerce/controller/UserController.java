33ded8c55f63fcbd09346a39a6401775d3c1ecfe
paesdv <paesm191@gmail.com>
2026-06-12 16:00:22 -0300

    getByEmail v1

--- src/main/java/com/estudo/ecommerce/controller/UserController.java
+++ src/main/java/com/estudo/ecommerce/controller/UserController.java
@@ -3,20 +3,19 @@ package com.estudo.ecommerce.controller;
 import com.estudo.ecommerce.model.dto.user.AlterarEmailRequest;
 import com.estudo.ecommerce.model.dto.user.AlterarSenhaRequest;
 import com.estudo.ecommerce.model.dto.user.RegisterRequestDTO;
+import com.estudo.ecommerce.model.dto.user.UserResponse;
 import com.estudo.ecommerce.model.entity.User;
 import com.estudo.ecommerce.repository.UserRepository;
 import com.estudo.ecommerce.service.AuthService;
 import com.estudo.ecommerce.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/usuario")
@@ -39,6 +38,14 @@ public class UserController {
         String emailAtual = auth.getName();
         userService.changePassword(emailAtual, request);
         return ResponseEntity.ok("Senha atualizada com sucesso!");
+    }
+
+    @GetMapping
+    public ResponseEntity<UserResponse> getByEmail(@RequestParam String email) {
+        return userService.getUserByEmail(email)
+                .map(UserResponse::toResponse)
+                .<ResponseEntity<UserResponse>>map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
